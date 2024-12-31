06652e886f3e63fcd4d421dbc82e1433b25d9451
Dharul <dharulmittal@gmail.com>
2024-12-31 22:54:45 +0530

    Cookie bug fixed

--- src/lib/util.js
+++ src/lib/util.js
@@ -7,7 +7,8 @@ export const generateToken = (username, res) => {
     res.cookie('jwt', token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        secure:true,
+        sameSite: "None",
     });
     return token;
 };
